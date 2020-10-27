d2e64862b22a0345f11d01f745b0878048eb12da
BenedictHomuth <56362368+BenedictHomuth@users.noreply.github.com>
2020-10-27 18:57:55 +0100

    Implemented UI Handling For Fight Page ✔
    You can now control the "user" stats with profile.js
    
    Also enemys are now clickable and can take damage with progressbar update -> fight.js

---
+++ frontend/js/profile.js
@@ -0,0 +1,83 @@
+//Here are global stats for the player
+
+var level = 5;
+var attackDamage = 10;
+var healthPoints = 100;
+var gold = 99;
+
+//On Start
+function initalize(){
+    console.log("Hello Profile here");
+
+    //Load User Stats
+
+    //Update Stats UI
+    updateLevelValue();
+    updateAttackDamage();
+    updateHealthPoints();
+    updateGoldValue();
+}
+initalize();
+
+
+
+
+//Display current Values
+function updateLevelValue(){
+    document.getElementById("userLevel").innerHTML = this.level;
+}
+
+function updateAttackDamage(){
+    document.getElementById("userAttackDamage").innerHTML = this.attackDamage;
+}
+
+function updateHealthPoints(){
+    document.getElementById("userHealth").innerHTML = this.healthPoints;
+}
+
+function updateGoldValue(){
+    document.getElementById("userGold").innerHTML = this.gold;
+}
+
+
+
+
+//Getter and Setter
+//Setter
+function setLevel(newLevel){
+    this.level = newLevel;
+    updateLevelValue();
+}
+
+function setAttackDamage(damageValue){
+    this.attackDamage = damageValue;
+}
+
+function setHealthPoints(healthValue){
+    this.healthPoints = healthValue;
+}
+
+function setgold(goldValue){
+    this.gold = goldValue;
+}
+
+//Getter
+function getLevel(newLevel){
+    return this.level;
+}
+
+function getAttackDamage(newLevel){
+    return this.attackDamage;
+}
+
+function getHealthPoints(newLevel){
+    return this.healthPoints;
+}
+
+function getUserGold(newLevel){
+    return this.gold;
+}
+
+
+
+
